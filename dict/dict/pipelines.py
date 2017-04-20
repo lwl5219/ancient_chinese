6d7ca24e22ff48ac088c376cf158d6e444b42dc1
lwl <lwl5219@gmail.com>
2017-04-21 07:26:40 +0800

    运行成功, 可以正确导出汉字

--- dict/dict/pipelines.py
+++ dict/dict/pipelines.py
@@ -7,5 +7,16 @@
 
 
 class DictPipeline(object):
+    idx = 0;
+
+    def open_spider(self, spider):
+        self.file = open('items.log', 'wb')
+        self.file.write("lwl open spider\n")
+
     def process_item(self, item, spider):
+        self.file.write("lwl process item %s \n" % item);
+        self.idx += 1
         return item
+
+    def close_spider(self, spider):
+        self.file.close()
--- dict/dict/settings.py
+++ dict/dict/settings.py
@@ -64,9 +64,11 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'dict.pipelines.DictPipeline': 300,
-#}
+#  ITEM_PIPELINES = {
+    #  'dict.pipelines.DictPipeline': 300,
+#  }
+
+FEED_EXPORT_ENCODING = 'utf-8'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
