73056d72db99e276c189f1a6a3453a0c3a721c56
lwl <lwl5219@gmail.com>
2018-06-21 22:03:21 +0800

    make dictionary script and base files

---
+++ ancient_chinese_dict/make_dict.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ $# != 1 ]];then
+    echo "Usage: ./make_dict.sh file.json"
+    exit 1
+fi
+
+file="ancient_chinese.json"
+cp $1 file
+# 处理scrapy爬取到的json数组
+sed -i "/^\[/d" $file
+sed -i "/^\]/d" $file
+sed -i "s/,$//" $file
+
+# 生成html文件
+python make_dict.py
+
+# 制作字典
+kindlegen Ancient_Chinese_Dict.opf
